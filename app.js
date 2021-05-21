c4f3b809fa5ff40c8cd4397cfa9a51afac8b040f
adribasn <adrien.basyn@outlook.com>
2021-05-21 12:45:38 +0200

    essential features

--- app.js
+++ app.js
@@ -1,3 +1,82 @@
 "use strict"
 
-const run
+const run = document.querySelector('#run');
+const stop = document.querySelector('#stop');
+
+let bulbs = document.getElementsByClassName('bulb');
+
+function dimBulbsDown() {
+    for (let i = 0; i < bulbs.length; i++) {
+        switch(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color')) {
+            case 'rgb(45, 255, 45)':
+                bulbs[i].classList.remove('animate-1');
+                break;
+            case 'rgb(255, 45, 45)':
+                bulbs[i].classList.remove('animate-2');
+                break;
+            case 'rgb(252, 255, 45)':
+                bulbs[i].classList.remove('animate-3');
+                break;
+            case 'rgb(45, 171, 255)':
+                bulbs[i].classList.remove('animate-4');
+                break;
+            default:
+                console.log(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color'));
+        }
+    }
+}
+
+function singleLoopBulbLightUp() {
+    /*
+    for (let i = 0; i < bulbs.length; i++) {
+        switch(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color')) {
+            case 'rgb(45, 255, 45)':
+                bulbs[i].classList.add('animate-1');
+                break;
+            case 'rgb(255, 45, 45)':
+                bulbs[i].classList.add('animate-2');
+                break;
+            case 'rgb(252, 255, 45)':
+                bulbs[i].classList.add('animate-3');
+                break;
+            case 'rgb(45, 171, 255)':
+                bulbs[i].classList.add('animate-4');
+                break;
+            default:
+                console.log(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color'));
+        }
+    }
+    */
+
+    for (let i = 0; i < bulbs.length; i++) {
+        lightBulbsUp(i);
+    }
+}
+
+function lightBulbsUp(i) {
+    setTimeout(function() {
+        switch(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color')) {
+            case 'rgb(45, 255, 45)':
+                bulbs[i].classList.add('animate-1');
+                break;
+            case 'rgb(255, 45, 45)':
+                bulbs[i].classList.add('animate-2');
+                break;
+            case 'rgb(252, 255, 45)':
+                bulbs[i].classList.add('animate-3');
+                break;
+            case 'rgb(45, 171, 255)':
+                bulbs[i].classList.add('animate-4');
+                break;
+            default:
+                console.log(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color'));
+        }
+    }, 500 * i);
+}
+
+
+stop.addEventListener('click', dimBulbsDown);
+run.addEventListener('click', singleLoopBulbLightUp);
+
+
+
