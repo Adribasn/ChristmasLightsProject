e16b1436ff397af9d2004695be0cee4b09b9991a
adribasn <adrien.basyn@outlook.com>
2021-05-23 07:59:15 +0200

    finished project

--- app.js
+++ app.js
@@ -1,82 +1,22 @@
 "use strict"
 
-const run = document.querySelector('#run');
-const stop = document.querySelector('#stop');
+const bulbs = document.getElementsByClassName('bulb');
+const run = document.getElementById('run');
+const stop = document.getElementById('stop');
 
-let bulbs = document.getElementsByClassName('bulb');
-
-function dimBulbsDown() {
+const on = function() {
     for (let i = 0; i < bulbs.length; i++) {
-        switch(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color')) {
-            case 'rgb(45, 255, 45)':
-                bulbs[i].classList.remove('animate-1');
-                break;
-            case 'rgb(255, 45, 45)':
-                bulbs[i].classList.remove('animate-2');
-                break;
-            case 'rgb(252, 255, 45)':
-                bulbs[i].classList.remove('animate-3');
-                break;
-            case 'rgb(45, 171, 255)':
-                bulbs[i].classList.remove('animate-4');
-                break;
-            default:
-                console.log(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color'));
-        }
+        bulbs[i].removeAttribute('style');
+        bulbs[i].animationPlayState = 'running';
     }
 }
 
-function singleLoopBulbLightUp() {
-    /*
+const off = function() {
     for (let i = 0; i < bulbs.length; i++) {
-        switch(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color')) {
-            case 'rgb(45, 255, 45)':
-                bulbs[i].classList.add('animate-1');
-                break;
-            case 'rgb(255, 45, 45)':
-                bulbs[i].classList.add('animate-2');
-                break;
-            case 'rgb(252, 255, 45)':
-                bulbs[i].classList.add('animate-3');
-                break;
-            case 'rgb(45, 171, 255)':
-                bulbs[i].classList.add('animate-4');
-                break;
-            default:
-                console.log(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color'));
-        }
-    }
-    */
-
-    for (let i = 0; i < bulbs.length; i++) {
-        lightBulbsUp(i);
+        bulbs[i].style.animation = 'none';
+        bulbs[i].style.background = '#301934';
     }
 }
 
-function lightBulbsUp(i) {
-    setTimeout(function() {
-        switch(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color')) {
-            case 'rgb(45, 255, 45)':
-                bulbs[i].classList.add('animate-1');
-                break;
-            case 'rgb(255, 45, 45)':
-                bulbs[i].classList.add('animate-2');
-                break;
-            case 'rgb(252, 255, 45)':
-                bulbs[i].classList.add('animate-3');
-                break;
-            case 'rgb(45, 171, 255)':
-                bulbs[i].classList.add('animate-4');
-                break;
-            default:
-                console.log(window.getComputedStyle(bulbs[i],null).getPropertyValue('background-color'));
-        }
-    }, 500 * i);
-}
-
-
-stop.addEventListener('click', dimBulbsDown);
-run.addEventListener('click', singleLoopBulbLightUp);
-
-
-
+run.addEventListener('click', on);
+stop.addEventListener('click', off);
